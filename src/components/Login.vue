f01dd138d7f2ecdf75c86368157d9c3206dba01c
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-13 22:32:53 +0700

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <form>
+        <div>
+            <label>Username:</label>
+            <input type="text" v-model="username"><br>
+            <label>Password:</label>
+            <input type="password" v-model="password"><br>
+            <button @click="loginHandle">Login</button><br>
+        </div>
+        <div>
+            <input type="checkbox">
+            <label>Remember me</label>
+            <router-link to="/forgotpassword">Forgot password?</router-link>
+        </div>
+    </form>
+    <!-- <p v-if="res">Hello {{ res.name }}!</p>
+    <p v-if="res">You are {{ res.age }} year olds.</p> -->
+  </template>
+  
+  <script setup>
+  import axios from 'axios';
+  import { ref } from 'vue'
+  
+  const res = ref('')
+  const username = ref('')
+  const password = ref('')
+  
+  function loginHandle() {
+    axios.get(``)
+      .then(function (response) {
+        res.value = response.data
+        console.log(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+  </script>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import(`../components/Home.vue`),
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(`../components/Login.vue`),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
