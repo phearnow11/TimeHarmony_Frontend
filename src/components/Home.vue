52762b3b743a4f5fea31504621df8f85c90d7ad0
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-15 18:19:13 +0700

    push to login if not login yet

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,9 +7,11 @@
 
 <script setup>
   import {useUserStore} from '../stores/user'
+  import router from '../router'
   const user = useUserStore()
-
-
+  if (user.user_id==null) {
+    router.push('/login')
+  }
 </script>
 
 <style></style>
