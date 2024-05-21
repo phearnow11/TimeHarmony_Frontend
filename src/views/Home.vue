58704e273563d3b10fa8a9d858886cdef7674601
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-21 21:13:19 +0700

    login gg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <div v-if="user">
+    {{ user.username }}
   </div>
 </template>
 
 <script setup>
+import { useUserStore } from '../stores/user';
+const user = useUserStore()
 </script>
 
 <style></style>
