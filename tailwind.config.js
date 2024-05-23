31734ae89f9632881dbb04ba51734f1bf64e73ff
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-23 08:54:41 +0700

    easd

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // Example content paths...
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue}',
     './src/components/*.{js,jsx,ts,tsx,vue}',
     './src/views/*.{js,jsx,ts,tsx,vue}',
     'node_modules/preline/dist/*.js',
-
   ],
   
   theme: {
