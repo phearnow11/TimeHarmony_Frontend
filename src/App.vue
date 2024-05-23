833369f3aea29728f0885f6afb7f24bca51d86a7
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-23 08:52:34 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -8,5 +8,5 @@
 </template>
 
 <style scoped>
-
+  
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,8 @@ export default {
     // Example content paths...
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx,vue}',
+    './src/components/*.{js,jsx,ts,tsx,vue}',
+    './src/views/*.{js,jsx,ts,tsx,vue}',
     'node_modules/preline/dist/*.js',
 
   ],
