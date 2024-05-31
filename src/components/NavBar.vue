a111a10ba1b8b43ee7c9af77c701de24a6b5153e
ThinhPhoenix <laithinh1310@gmail.com>
2024-05-31 17:39:29 +0700

    da

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -23,8 +23,7 @@
       <div class="ui-input-icon">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
           <path
-            stroke-linejoin="round"
-            stroke-linecap="round"
+            
             stroke-width="2"
             stroke="currentColor"
             d="M21 21L16.65 16.65M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
