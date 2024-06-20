6fb547aff9f41fdb3e2869bd539d230fbbbb7166
DavidN2076 <nhancd909@gmail.com>
2024-06-20 15:43:13 +0700

    Configure upload and detail

--- src/stores/watch.js
+++ src/stores/watch.js
@@ -66,6 +66,45 @@ export const useWatchStore = defineStore("watch", {
     
 
 
+    async loadWatch(watchData) {
+      try {
+        // Update the state with user data
+        this.watch_data.name = watchData.name ? watchData.name : null;
+        this.watch_data.watch_description = watchData.description ? watchData.description : null;
+        this.watch_data.price = watchData.price ? watchData.price : null;
+        this.watch_data.brand = watchData.brand ? watchData.brand : null;
+        this.watch_data.series = watchData.series ? watchData.series : null;
+        this.watch_data.model = watchData.model ? watchData.model : null;
+        this.watch_data.gender = watchData.gender ? watchData.gender : null;
+        this.watch_data.style = watchData.style ? watchData.style : null;
+        this.watch_data.subclass = watchData.subclass ? watchData.subclass : null;
+        this.watch_data.madelabel = watchData.madelabel ? watchData.madelabel : null;
+        this.watch_data.calender = watchData.calender ? watchData.calender : null;
+        this.watch_data.feature = watchData.feature ? watchData.feature : null;
+        this.watch_data.movement = watchData.movement ? watchData.movement : null;
+        this.watch_data.functions = watchData.functions ? watchData.functions : null;
+        this.watch_data.engine = watchData.engine ? watchData.engine : null;
+        this.watch_data.waterresistant = watchData.waterresistant ? watchData.waterresistant : null;
+        this.watch_data.bandcolor = watchData.bandcolor ? watchData.bandcolor : null;
+        this.watch_data.bandtype = watchData.bandtype ? watchData.bandtype : null;
+        this.watch_data.clasp = watchData.clasp ? watchData.clasp : null;
+        this.watch_data.bracelet = watchData.bracelet ? watchData.bracelet : null;
+        this.watch_data.dialtype = watchData.dialtype ? watchData.dialtype : null;
+        this.watch_data.dialcolor = watchData.dialcolor ? watchData.dialcolor : null;
+        this.watch_data.crystal = watchData.crystal ? watchData.crystal : null;
+        this.watch_data.secondmaker = watchData.secondmaker ? watchData.secondmaker : null;
+        this.watch_data.bezel = watchData.bezel ? watchData.bezel : null;
+        this.watch_data.bezel_material = watchData.bezelmaterial ? watchData.bezelmaterial : null;
+        this.watch_data.caseback = watchData.caseback ? watchData.caseback : null;
+        this.watch_data.casedimension = watchData.casedimension ? watchData.casedimension : null;
+        this.watch_data.caseshape = watchData.caseshape ? watchData.caseshape : null;
+
+
+
+      } catch (err) {
+        console.error("Error fetching member data:", err);
+      }
+    },
     async getDetailWatch(watch_id) {
       
       try {
@@ -104,14 +143,16 @@ export const useWatchStore = defineStore("watch", {
         }).catch((error) =>{})
         // Update the state with user data
         
-
-
-
+  
+  
+  
       } catch (err) {
         console.error("Error fetching member data:", err);
       }
     },
   },
+
+  
 
 
 });
